b167e5d752deb9050b2d1fa89e5eefd2c8e1f4bc
ff <ff>
2022-03-09 20:32:51 +0800

    22-3-9

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update///
-    void Start()
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+
+    [SerializeField] private float moveSpeed;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        moveV = Input.GetAxis("Vertical") * moveSpeed;
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+
+    private void Flip()
+    {
+        if (moveH > 0) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (moveH < 0) transform.eulerAngles = new Vector3(0, 180, 0);
     }
 }
+
